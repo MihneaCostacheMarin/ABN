820951bea499c34440433a11f85bd162f89be6b3
Mihnea <mihneacostachemarin@gmail.com>
2023-03-06 12:21:50 +0200

    Commit to add swagger

--- src/main/java/org/abn/Main.java
+++ src/main/java/org/abn/Main.java
@@ -2,12 +2,15 @@ package org.abn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
 }
